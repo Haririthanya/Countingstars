9fc985e3001d271bbefe100b0e1265fe42ff69ad
Haririthanya <41746215+Haririthanya@users.noreply.github.com>
2019-01-29 14:41:34 +0530

    Add files via upload

---
+++ Count.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from matplotlib import pyplot as plt
+from skimage import data
+from skimage.feature import blob_dog, blob_log, blob_doh
+from math import sqrt
+from skimage.color import rgb2gray
+import glob
+from skimage.io import imread
+
+
+# In[14]:
+
+
+example_file = glob.glob(r"C:/Users/Lenovo/Desktop/sky2.jpg")[0]
+im = imread(example_file, as_grey=True)
+cm_gray = plt.get_cmap('gray')
+plt.imshow(im, cmap=cm_gray)
+plt.show()
+
+
+# In[15]:
+
+
+blobs_log = blob_log(im, max_sigma=30, num_sigma=10, threshold=.1)
+# Compute radii in the 3rd column.
+blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
+numrows = len(blobs_log)
+print("Number of stars counted : " ,numrows)
+
+
+# In[16]:
+
+
+fig, ax = plt.subplots(1, 1)
+plt.imshow(im, cmap=cm_gray)
+for blob in blobs_log:
+    y, x, r = blob
+    c = plt.Circle((x, y), r+5, color='lime', linewidth=2, fill=False)
+    ax.add_patch(c)
+
